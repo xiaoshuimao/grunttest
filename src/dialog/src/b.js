6be8d41353719edcb05dd17f2bced47ae8118e19
章 <403724532@qq.com>
2014-08-26 17:16:30 +0800

    Revert "Signed-off-by: ç«? <403724532@qq.com>"
    This reverts commit 27e41685dee4c5e8476e0cab309acf5cc5060585.

--- src/dialog/src/b.js
+++ src/dialog/src/b.js
@@ -1,8 +1,7 @@
 define(function(require, exports, module) {
-
   function B() {
     console.log('B')
-  };
+  }
 
   module.exports = B;
 })
--- src/dialog/src/dialog.js
+++ src/dialog/src/dialog.js
@@ -1,8 +1,6 @@
 define(function(require, exports, module) {
-
     var $ = require("jquery");
     var B = require("./b");
-    
     require("./dialog_css.css");
     
     new B();
