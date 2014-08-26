27e41685dee4c5e8476e0cab309acf5cc5060585
章 <403724532@qq.com>
2014-08-26 15:10:50 +0800

    Signed-off-by: ç«? <403724532@qq.com>

--- src/dialog/src/b.js
+++ src/dialog/src/b.js
@@ -1,7 +1,8 @@
 define(function(require, exports, module) {
+
   function B() {
     console.log('B')
-  }
+  };
 
   module.exports = B;
 })
--- src/dialog/src/dialog.js
+++ src/dialog/src/dialog.js
@@ -1,6 +1,8 @@
 define(function(require, exports, module) {
+
     var $ = require("jquery");
     var B = require("./b");
+    
     require("./dialog_css.css");
     
     new B();
